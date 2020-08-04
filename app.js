0815b0044de0cf2c48bf400986a74d04c6bf551c
Eric Park <epark0995@gmail.com>
2020-08-04 11:41:06 -0700

    yelp function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 $('#search').click(() => {
     event.preventDefault()
     let stock  = $('#stock').val().toUpperCase()
+    let restaurant = $('#food').val()
     console.log(stock)
     axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${stock}&token=bsk4nnvrh5rachpnrlt0`)
     .then(res => {
@@ -29,3 +30,27 @@ $('#search').click(() => {
         console.log(err)
     })
 })
+
+function search3money() {
+    
+    
+axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=3&limit=5`, {
+      headers: {
+        'Authorization': 'Bearer gSvqKHVo4SqT2L7QNABBlQQNcVJp8c16F0RBXZHqryC6qvjTgTbDHpTnN1ltdJDPeunYBd_rxAersy_heJPvUmjxnhJhbay0xdwJp0hMtCfwxyt-JntJ-62AjHgoX3Yx'
+      }
+    })
+      .then(res => {
+        console.log(res)
+        for (let i = 0; i < res.data.businesses.length; i++) {
+            let businessElem = document.createElement('div')
+            businessElem.innerHTML = `
+            <p>
+                <img src = "${res.data.businesses[i]}.image_url)">
+                </p>
+                `}
+        })
+      .catch(err => console.error(err)) }
+
+    
+
+
