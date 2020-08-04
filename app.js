f80dd0117f0769f3d9b81d0fcaf1ecf70f9d77d9
Eric Park <epark0995@gmail.com>
2020-08-04 12:51:44 -0700

    added price functions

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //bsk4nnvrh5rachpnrlt0 finnhub key
 //gSvqKHVo4SqT2L7QNABBlQQNcVJp8c16F0RBXZHqryC6qvjTgTbDHpTnN1ltdJDPeunYBd_rxAersy_heJPvUmjxnhJhbay0xdwJp0hMtCfwxyt-JntJ-62AjHgoX3Yx yelp key
+
+//Make functions for all $ sign range and to list by rating 
+
+
 $('#search').click(() => {
     event.preventDefault()
     let stock = $('#stock').val().toUpperCase()
@@ -35,35 +39,62 @@ $('#search').click(() => {
 })
 
 
-//search 3 dollar sign businesses (limit 5)
-// function search3Dolla () {
-//     let restaurant = $('#food').val()
-//     console.log(restaurant)
-//     axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=3&limit=5`, {
-//     headers: {
-//     'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
-//     }
-//     })
-//         .then(res => {
-//             console.log(res)
-//             for (let i = 0; i < business.length; i++) {
-//                 let businessElem = document.createElement('div')
-//                 console.log(business[0].image_url)
 
-//                 businessElem.innerHTML = `
-//                 <p>
-//                     <img src='${business[i].image_url}'>
-//                 </p>
-//                 `
-//             }
-//         })
-//         .catch(err => {
-//             console.error(err)
-//         })
-// }
+function search1Price() {
+    let restaurant = $('#food').val()
+    console.log(restaurant)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=1&limit=5`, {
+    headers: {
+    'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
+    }
+    })
+        .then(res => {
+            console.log(res)
+            for (let i = 0; i < res.data.businesses.length; i++) {
+                //console.log(res.data.businesses[i].image_url)
+                $('#foodSearch').append(`
+                 <img class="picture" src= "${res.data.businesses[i].image_url}">
+                <a href="${res.data.businesses[i].url}">Name: ${res.data.businesses[i].name}</a>
+                <p>Type: ${res.data.businesses[i].categories[0].title}</p>
+                <p>Rating: ${res.data.businesses[i].rating}</p>
+                <p>Review Count: ${res.data.businesses[i].review_count}</p>
+                `)
+            }
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    $('#foodSearch').empty()
+}
 
-$('#search2').click(() => {
-    event.preventDefault ()
+function search2Price() {
+    let restaurant = $('#food').val()
+    console.log(restaurant)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=2&limit=5`, {
+    headers: {
+    'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
+    }
+    })
+        .then(res => {
+            console.log(res)
+            for (let i = 0; i < res.data.businesses.length; i++) {
+                //console.log(res.data.businesses[i].image_url)
+                $('#foodSearch').append(`
+                 <img class="picture" src= "${res.data.businesses[i].image_url}">
+                <a href="${res.data.businesses[i].url}">Name: ${res.data.businesses[i].name}</a>
+                <p>Type: ${res.data.businesses[i].categories[0].title}</p>
+                <p>Rating: ${res.data.businesses[i].rating}</p>
+                <p>Review Count: ${res.data.businesses[i].review_count}</p>
+                `)
+            }
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    $('#foodSearch').empty()
+}
+
+function search3Price() {
     let restaurant = $('#food').val()
     console.log(restaurant)
     axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=3&limit=5`, {
@@ -76,9 +107,7 @@ $('#search2').click(() => {
             for (let i = 0; i < res.data.businesses.length; i++) {
                 //console.log(res.data.businesses[i].image_url)
                 $('#foodSearch').append(`
-                <p>
-                    <img class="picture" src= "${res.data.businesses[i].image_url}">
-                </p>
+                 <img class="picture" src= "${res.data.businesses[i].image_url}">
                 <a href="${res.data.businesses[i].url}">Name: ${res.data.businesses[i].name}</a>
                 <p>Type: ${res.data.businesses[i].categories[0].title}</p>
                 <p>Rating: ${res.data.businesses[i].rating}</p>
@@ -90,28 +119,34 @@ $('#search2').click(() => {
             console.error(err)
         })
     $('#foodSearch').empty()
-})
+}
+
+function search4Price() {
+    let restaurant = $('#food').val()
+    console.log(restaurant)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=4&limit=5`, {
+    headers: {
+    'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
+    }
+    })
+        .then(res => {
+            console.log(res)
+            for (let i = 0; i < res.data.businesses.length; i++) {
+                //console.log(res.data.businesses[i].image_url)
+                $('#foodSearch').append(`
+                 <img class="picture" src= "${res.data.businesses[i].image_url}">
+                <a href="${res.data.businesses[i].url}">Name: ${res.data.businesses[i].name}</a>
+                <p>Type: ${res.data.businesses[i].categories[0].title}</p>
+                <p>Rating: ${res.data.businesses[i].rating}</p>
+                <p>Review Count: ${res.data.businesses[i].review_count}</p>
+                `)
+            }
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    $('#foodSearch').empty()
+}
 
 
-// function search3Dolla () {
-//     event.preventDefault()
-//     let restaurant = $('#food').val()
-//     console.log(restaurant)
-//     axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${restaurant}&price=3&limit=5`, {
-//     headers: {
-//     'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
-//     }
-//     })
-//         .then(res => {
-//             console.log(res)
-//             for (let i = 0; i < business.length; i++) {
-//                 let businessElem = document.createElement('div')
-
-//                 businessElem.innerHTML
-//             }
-//         })
-//         .catch(err => {
-//             console.error(err)
-//         })
-// }
 
